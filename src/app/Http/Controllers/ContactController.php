457050d381fa6cbfec2b6a729550ce9c04758316
root <root@DESKTOP-61L25GU>
2024-03-20 23:08:39 +0900

    index->confirm連携OK

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -12,12 +13,13 @@ class ContactController extends Controller
     }
     public function confirm(Request $request)
     {
-        $contact = $request->only(['first_name', 'gender', 'email', 'tel1', 'address', 'building', 'content-type', 'detail']);
+        $contact = $request->only(['first_name', 'last_name', 'gender', 'email', 'tel1', 'tel2', 'tel3', 'address', 'building', 'content-type', 'detail']);
         return view('confirm', compact('contact'));
     }
     public function store(Request $request)
     {
         $contact = $request->only(['first_name', 'gender', 'email', 'tel1', 'address', 'building', 'content-type', 'detail']);
-        return view('thanks', compact('contact'));
+        Contact::create($contact);
+        return view('thanks');
     }
 }
--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -16,7 +16,7 @@
                 <tr class="confirm-table__row">
                     <th class="confirm-table__header">お名前</th>
                     <td class="confirm-table__text">
-                        <input type="text" name="first_name" value="{{$contact['first_name']}}" readonly />
+                        <input type="text" name="name" value="{{$contact['first_name'].' '.$contact['last_name']}}" readonly />
                     </td>
                 </tr>
                 <tr class="confirm-table__row">
@@ -34,7 +34,7 @@
                 <tr class="confirm-table__row">
                     <th class="confirm-table__header">電話番号</th>
                     <td class="confirm-table__text">
-                        <input type="text" name="tel1" value="{{$contact['tel1']}}" readonly />
+                        <input type="text" name="tel" value="{{$contact['tel1'].$contact['tel2'].$contact['tel3']}}" readonly />
                     </td>
                 </tr>
                 <tr class="confirm-table__row">
